9adc5322067ffc7cde25e98c1769d892da8f871c
serdal <ozcetinserdal@gmail.com>
2023-08-05 00:53:55 +0300

    first commit

---
+++ src/test/java/deneme.java
@@ -0,0 +1,32 @@
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class deneme {
+
+
+    AndroidDriver driver;
+
+    @Test
+    public void deneme01() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\asus\\IdeaProjects\\Appium\\apps\\Calculator_8.4.1 (520193683)_Apkpure.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+    }
+}
