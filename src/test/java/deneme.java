8f04b6368319230daec2b601ee41e5fb3a54b235
Selma0204 <selmaeren.0204@gmail.com>
2023-08-16 12:03:24 +0300

    selma

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -1,8 +1,10 @@
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
